72c8997ce7da88205e674a3697c7517371980af1
itgsod-daniel-kallberg <daniel.kallberg@elev.it-gymnasiet.se>
2015-03-24 14:53:48 +0100

    Hey Ignore me please

--- dices/hello.rb
+++ dices/hello.rb
@@ -3,10 +3,10 @@ require 'colorize'
 $hash = Hash.new
 
 for i in 1..20
-  $ash["#{i}"] = rand(1000)
+  $hash["#{i}"] = rand(1000)
 end
 
 puts "Bosse did /ignore on me so he doesn't have to help me and so he can't hear me shouting for his help".blue.blink
 
-puts "I made it by copying Oscar".blink
+puts "I made it by copying Oscar".red.blink
 
