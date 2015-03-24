6e50d1a7c3aaf1cf56e0db90259bd250581e9593
itgsod-daniel-kallberg <daniel.kallberg@elev.it-gymnasiet.se>
2015-03-24 14:51:28 +0100

    Hey Ignore me please

---
+++ dices/hello.rb
@@ -0,0 +1,12 @@
+require 'colorize'
+
+$hash = Hash.new
+
+for i in 1..20
+  $ash["#{i}"] = rand(1000)
+end
+
+puts "Bosse did /ignore on me so he doesn't have to help me and so he can't hear me shouting for his help".blue.blink
+
+puts "I made it by copying Oscar".blink
+
